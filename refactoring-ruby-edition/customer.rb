3e999253e72665321209c3b9abf9fb0211d3cf06
Junhyung Jung <junhyung.jung@cre.ma>
2018-10-15 23:47:47 +0900

    5. 임시변수 없애기     - 임시변수를 메서드 호출로 변환 기법     - 루프를 컬렉션 클로저 메서드로 전환 (Replace Loop with Collection Closure Method)     - total_charge, total_frequent_rental_points

--- refactoring-ruby-edition/customer.rb
+++ refactoring-ruby-edition/customer.rb
@@ -12,21 +12,24 @@ class Customer
   end
 
   def statement
-    total_amount = 0
-    frequent_renter_points = 0
     result = "고객 #{@name}의 대여 기록\n"
 
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-
       # 이번 대여의 계산 결과를 표시
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge
     end
 
     # 푸터 행 추가
-    result += "대여료는 #{total_amount}입니다.\n"
-    result += "적립 포인트는 #{frequent_renter_points}입니다."
+    result += "대여료는 #{total_charge}입니다.\n"
+    result += "적립 포인트는 #{total_frequent_rental_points}입니다."
     result
+  end
+
+  def total_charge
+    @rentals.inject(0) {|sum, rental| sum + rental.charge}
+  end
+
+  def total_frequent_rental_points
+    @rentals.inject(0) {|sum, rental| sum + rental.frequent_renter_points}
   end
 end
